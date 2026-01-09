e2fa042682ff29ecde3951b9e6d7a5badbb6413d
David Gilman <dgilman@aidentified.com>
2026-01-09 14:47:54 -0500

    package updates

--- pip-upgrade.sh
+++ pip-upgrade.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-pip-compile --output-file requirements.txt requirements.in --upgrade --allow-unsafe
-pip-compile --output-file requirements-dev.txt requirements-dev.in --upgrade --allow-unsafe
+uv pip compile --output-file requirements.txt requirements.in --upgrade
+uv pip compile --output-file requirements-dev.txt requirements-dev.in --upgrade
